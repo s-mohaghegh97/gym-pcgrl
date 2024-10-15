8cea0c24548bf62b6034736bf31aa7c784326cea
SK2iP <sadra.khanjari@gmail.com>
2024-10-15 14:13:16 +0330

    fix create_data_set and add wide datasets

--- create_dataset.py
+++ create_dataset.py
@@ -1,6 +1,3 @@
-"""
-Run a trained agent and get generated maps
-"""
 import os
 import pickle
 import random
@@ -26,137 +23,109 @@ class FullyConvPolicyBigMap(FeedForwardPolicy):
         super(FullyConvPolicyBigMap, self).__init__(*args, **kwargs,
                                                    net_arch=[dict(pi=[64, 64], vf=[64, 64])])
 
-
-def reset_data():
-    return {'observations': [],
-            'actions': [],
-            'terminals': [],
-            'rewards': [],
-            'next_observations': []
-            }
-
-
-def append_data(data, s, a, r, new_s, done):
-    data['observations'].append(s)
-    data['actions'].append(a)
-    data['rewards'].append(r)
-    data['terminals'].append(done)
-    data['next_observations'].append(new_s)
-
-
-def npify(data):
-    for k in data:
-        if k == 'terminals':
-            dtype = np.bool_
-        else:
-            dtype = np.float32
-
-        data[k] = np.array(data[k], dtype=dtype)
-
-
-# def show_state(env, step=0, changes=0, total_reward=0, name=""):
-#     plt.figure(10)
-#     plt.clf()
-#     plt.title("{} | Step: {} Changes: {} Total Reward: {}".format(name, step, changes, total_reward))
-#     plt.axis('off')
-#     # plt.imshow(env.render(mode='rgb_array'))
-#     # plt.savefig(f"./runs/{step}.jpg")
-#     # plt.show()
-#     display.clear_output(wait=True)
-#     display.display(plt.gcf())
-
-
-def infer(game, representation, model_path, num_episode):
-    """
-     - max_trials: The number of trials per evaluation.
-     - infer_kwargs: Args to pass to the environment.
-    """
-    policy = CustomPolicyBigMap
-
-    agent = PPO2.load(model_path, custom_objects={
-        "FullyConvPolicy": FullyConvPolicyBigMap,
-        "FullyConvPolicySmallMap": FullyConvPolicySmallMap,
-        "CustomPolicyBigMap": CustomPolicyBigMap,
-        "CustomPolicySmallMap": CustomPolicySmallMap
-    })
-    # dir_path = r"./runs"
-    # shutil.rmtree(dir_path, ignore_errors=True)
-    # os.mkdir(dir_path)
-    episode = 0
-    data = []
-    while episode < num_episode:
-        print(episode)
-        data_episodes = reset_data()
-        kwargs = {
-            'change_percentage': random.random(),
-            'verbose': False,
-        }
-        env_name = '{}-{}-v0'.format(game, representation)
+class Inference:
+    def __init__(self, game, representation):
+        # Set dimensions based on game and representation
         if game == "binary":
-            model.FullyConvPolicy = model.FullyConvPolicyBigMap
-            kwargs['cropped_size'] = 28
+            self.dim = 2
+            self.width = 14
+            self.height = 14
+            self.length = 'path-length'
         elif game == "zelda":
-            model.FullyConvPolicy = model.FullyConvPolicyBigMap
-            kwargs['cropped_size'] = 22
+            self.dim = 8
+            self.width = 11
+            self.height = 7
+            self.length = 'path-length'
+
         elif game == "sokoban":
-            model.FullyConvPolicy = model.FullyConvPolicySmallMap
-            kwargs['cropped_size'] = 10
-        kwargs['render'] = False
-        env = make_vec_envs(env_name, representation, None, 1, **kwargs)
-        obs = env.reset()
-        # show_state(env)
-        # obs_original = env.ori
-        dones = False
-        total_rewards = 0
-        old_change = 0
-        while not dones:
-            action, _ = agent.predict(obs)
-            new_obs, rewards, dones, info = env.step(action)
-            h=5
-            w=5
-            dim=5
-            _, _, action = np.unravel_index(action[0], (h, w, dim))
-            # print(info)
-            info = info[0]
-            # show_state(env, info['iterations'], info['changes'], total_rewards)
-            if (info["changes"] != old_change) or (dones and info["path-length"] > 18 and info["changes"] != old_change):
-                print(info)
-                append_data(data_episodes, obs, action, int(rewards), new_obs, dones)
-                print(info)
-                if dones and info["path-length"] > 18:
-                    # print("finish")
+            self.dim = 5
+            self.width = 5
+            self.height = 5
+            self.length = 'sol-length'
+        else:
+            raise ValueError("Unsupported game type")
+
+
+    def reset_data(self):
+        return {'observations': [],
+                'actions': [],
+                'terminals': [],
+                'rewards': [],
+                'next_observations': []
+                }
+
+    def append_data(self, data, s, a, r, new_s, done):
+        data['observations'].append(s)
+        data['actions'].append(a)
+        data['rewards'].append(r)
+        data['terminals'].append(done)
+        data['next_observations'].append(new_s)
+
+    def infer(self, game, representation, model_path, num_episode):
+        # policy = CustomPolicyBigMap
+        agent = PPO2.load(model_path)
+        episode = 0
+        data = []
+        while episode < num_episode:
+            print(episode)
+            data_episodes = self.reset_data()
+            kwargs = {
+                'change_percentage': random.random(),
+                'verbose': False,
+            }
+            env_name = '{}-{}-v0'.format(game, representation)
+            if game == "binary":
+                model.FullyConvPolicy = model.FullyConvPolicyBigMap
+                kwargs['cropped_size'] = 28
+            elif game == "zelda":
+                model.FullyConvPolicy = model.FullyConvPolicyBigMap
+                kwargs['cropped_size'] = 22
+            elif game == "sokoban":
+                model.FullyConvPolicy = model.FullyConvPolicySmallMap
+                kwargs['cropped_size'] = 10
+            kwargs['render'] = False
+            env = make_vec_envs(env_name, representation, None, 1, **kwargs)
+            obs = env.reset()
+            dones = False
+            total_rewards = 0
+            old_change = 0
+            while not dones:
+                action, _ = agent.predict(obs)
+                new_obs, rewards, dones, info = env.step(action)
+                print(action)
+                _, _, action = np.unravel_index(action[0], (self.height, self.width, self.dim))
+                info = info[0]
+                if (info["changes"] != old_change) or (dones and info[ self.length] > 18 and info["changes"] != old_change):
+                    self.append_data(data_episodes, obs, action, int(rewards), new_obs, dones)
+                    if dones and info[ self.length] > 18:
+                        break
+                obs = new_obs
+                total_rewards += rewards
+                old_change = info["changes"]
+                if dones:
                     break
-            obs = new_obs
-            total_rewards += rewards
-            old_change = info["changes"]
-            if dones:
-                break
-        if dones and info["path-length"] >= 18:
-            data_episodes['observations'] = np.concatenate([np.array(data_episodes['observations'])], axis=0)
-            data_episodes['next_observations'] = np.concatenate([np.array(data_episodes['next_observations'])],
-                                                                axis=0)
-            data_episodes['actions'] = np.concatenate([np.array(data_episodes['actions'])], axis=0)
-            data_episodes['terminals'] = np.concatenate([np.array(data_episodes['terminals'])], axis=0)
-            data_episodes['rewards'] = np.concatenate([np.array(data_episodes['rewards'])], axis=0)
+            if dones and info[ self.length] >= 18:
+                data_episodes['observations'] = np.concatenate([np.array(data_episodes['observations'])], axis=0)
+                data_episodes['next_observations'] = np.concatenate([np.array(data_episodes['next_observations'])], axis=0)
+                data_episodes['actions'] = np.concatenate([np.array(data_episodes['actions'])], axis=0)
+                data_episodes['terminals'] = np.concatenate([np.array(data_episodes['terminals'])], axis=0)
+                data_episodes['rewards'] = np.concatenate([np.array(data_episodes['rewards'])], axis=0)
 
-            data.append(data_episodes)
-            episode += 1
-        if kwargs.get('verbose', False):
-            print(info)
-    fname = 'sokoban_wide_100000.pkl'
-    with open(fname, 'wb') as f:
-        print('Before dump')
-        pickle.dump(data, f)
-
+                data.append(data_episodes)
+                episode += 1
+        fname = f'./dataset/{game}_{representation}_dataset.pkl'
+        if not os.path.exists('./dataset'):  
+            os.mkdir('./dataset')
+        with open(fname, 'wb') as f:
+            print('Before dump')
+            pickle.dump(data, f)
 
 ################################## MAIN ########################################
-games = [] 
-game = 'binary'
-representation = 'wide'
-# model_path = 'models/{}/{}/model_1.pkl'.format(game, representation)
-model_path = './models/binary/wide/model_1.pkl'
-num_episode = 1
-
-
 if __name__ == '__main__':
-    infer(game, representation, model_path, num_episode)
+    game = 'zelda'
+    representation = 'wide'
+    model_path = 'runs/{}_{}_1_log/latest_model.pkl'.format(game, representation)
+    num_episode = 1
+
+    inference = Inference(game, representation)
+    inference.infer(game, representation, model_path, num_episode)
